45a70100ad95781be2baaa00612456d11117946f
muthita munmueang <muthita.mu.61@ubu.ac.th>
2020-10-13 23:18:24 +0700

    Updates

---
+++ precache-manifest.489607230852469bb0fdcc80db8c7bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0944373276fee5b81e189adb2245cc",
+    "url": "/com-project/index.html"
+  },
+  {
+    "revision": "ae71241292889ca090dc",
+    "url": "/com-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eea0795be984e84b72d0",
+    "url": "/com-project/static/js/2.387bb0c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/com-project/static/js/2.387bb0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae71241292889ca090dc",
+    "url": "/com-project/static/js/main.0baa2b79.chunk.js"
+  },
+  {
+    "revision": "515dacc7bde1528d9606",
+    "url": "/com-project/static/js/runtime-main.38a47eca.js"
+  }
+]);
